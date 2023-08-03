317e88157758c48e77b6092cca76fe039b9d9c28
Keks <keks@htmlacademy.ru>
2023-08-03 17:22:02 +0300

    :heavy_check_mark: Сборка #2

--- 2/js/functions.js
+++ 2/js/functions.js
@@ -1,47 +1,59 @@
 //Первая функция: Палиндром
+
 const isPalindrom = (string) => {
+
   for (let i = 0, j = string.length - 1; i < j; i++, j--) {
     if(string[i] !== string[j]) {
       return false;
     }
   }
+
   return true;
 };
+
 isPalindrom ('кекс');
 
 //Вторая функция: Извлечение цифр
-const numberFunction = (string) => {
-  if (typeof string === 'number') {
-    return string;
+
+const getNumberFunction = (someString) => {
+  if (typeof someString === 'number') {
+    return someString;
   }
+
   let selection = '';
-  for(let i = 0; i <= string.length; i++) {
-    if(!Number.isNaN(parseInt(string.at(i), 10))) {
-      selection += string.at(i);
+  for(let i = 0; i <= someString.length; i++) {
+    if(!Number.isNaN(parseInt(someString.at(i), 10))) {
+      selection += someString.at(i);
     }
   }
+
   return parseInt(selection, 10);
 };
-numberFunction('1 кефир, 0.5 батона');
+
+getNumberFunction('1 кефир, 0.5 батона');
 
 
 //Третья функция, которая возвращает исходную строку, дополненную указанными символами до заданной длины
-function symbolFunction (initialString, minimalLength, addSymbol) {
+
+const getSymbolFunction = (initialString, minimalLength, addSymbol) => {
   const resultAdd = minimalLength - initialString.length;
+
   if (resultAdd <= 0) {
     return initialString;
   }
 
   const repeatCount = Math.floor(resultAdd / addSymbol.length);
   const endLength = resultAdd % addSymbol.length;
+
   return addSymbol.slice(0, endLength) + addSymbol.repeat(repeatCount) + initialString;
-}
-symbolFunction('q', 4, 'we');
+};
+
+getSymbolFunction('q', 4, 'we');
 
 
 //Четвертая функция (Кексобукинг), возвращающая случайное число с плавающей точкой из переданного диапазона включительно
 
-function randomNumberFunction (minValue, maxValue, numberPoint) {
+const getRandomNumber = (minValue, maxValue, numberPoint) => {
   if (maxValue < minValue) {
     [minValue, maxValue] = [maxValue, minValue];
   }
@@ -53,6 +65,6 @@ function randomNumberFunction (minValue, maxValue, numberPoint) {
   const randomNumber = minValue + Math.random() * (maxValue - minValue);
 
   return Number(randomNumber.toFixed(numberPoint));
-}
+};
 
-randomNumberFunction(100, 200, 2);
+getRandomNumber(100, 200, 2);
